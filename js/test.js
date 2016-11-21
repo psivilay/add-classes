f7fb896726607be48b9f191044d87b3aa8700594
Peter Sivilay <sivilaypeter@gmail.com>
2016-11-20 21:34:13 -0600

    Add working buttons to change background color

--- js/test.js
+++ js/test.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
   $("button#green").click(function() {
-    $("body").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
-    $("body").removeClass();
     $("body").addClass("yellow-background");
   });
 
   $("button#red").click(function() {
-    $("body").removeClass();
     $("body").addClass("red-background");
   });
 });
